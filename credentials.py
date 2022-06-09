5cada737d747c7d0b08de2304ebda57b7aea23f4
wagnerlmichael <wagnerleemichael@gmail.com>
2022-06-08 21:39:57 -0500

    ignore file add

---
+++ credentials.py
@@ -0,0 +1,18 @@
+import os
+
+api_credentials = {
+    'my_api_key': "6ZpFREngHMhbBmhYmHjmQ5U6x",
+    'my_api_secret': "Yf1i6GAKiC1UCpLY30NuYNMKz27zUh3jPVCFliDdN0G20AZqH6",
+    'bearer_token': 'AAAAAAAAAAAAAAAAAAAAAL37cwEAAAAAhMnGRSCAyPnzUQrPswnMrpFE9rQ',
+    'access_token': '1051382436-5ny3cHGQt4R0pUGjxB1HHNfwE3qqeUJ87G9xwjk',
+    'access_token_secret': 'q0wQ2Gm0OG7UR5SuhEfInnE6f6lo0Dg2Fr8PhpAq2ElsT'
+}
+
+"""
+my_api_key = '6ZpFREngHMhbBmhYmHjmQ5U6x'
+my_api_secret = 'Yf1i6GAKiC1UCpLY30NuYNMKz27zUh3jPVCFliDdN0G20AZqH6'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAL37cwEAAAAAhMnGRSCAyPnzUQrPswnMrpFE9rQ%3D55GuFAkKIhTiRBq9xCvo429xq2KwbSFHt4x7E1kVrOuP5ZZcu4'
+access_token = '1051382436-5ny3cHGQt4R0pUGjxB1HHNfwE3qqeUJ87G9xwjk'
+access_token_secret = 'q0wQ2Gm0OG7UR5SuhEfInnE6f6lo0Dg2Fr8PhpAq2ElsT'
+"""
+
